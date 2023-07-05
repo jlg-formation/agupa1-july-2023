aed8a4e910d9df66ad328ca5b1e2c222ade82a17
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-05 14:27:57 +0200

    dev: routing step 3/4
    added router-outlet

--- front/src/app/layout/layout.module.ts
+++ front/src/app/layout/layout.module.ts
@@ -3,22 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
-
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent,
-    BodyComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    HeaderComponent,
-    FooterComponent,
-    BodyComponent
-  ]
+  declarations: [HeaderComponent, FooterComponent, BodyComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [HeaderComponent, FooterComponent, BodyComponent],
 })
-export class LayoutModule { }
+export class LayoutModule {}
