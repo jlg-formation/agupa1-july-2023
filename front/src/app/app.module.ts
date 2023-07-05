7e7fb4b22fa5aaa7d581ff41cb076d125e24a662
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-05 14:02:52 +0200

    dev: routing step 1/4
    created 2 components
    command:
    
    npx ng g c routes/home
    npx ng g c routes/legal

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { HomeComponent } from './routes/home/home.component';
+import { LegalComponent } from './routes/legal/legal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LegalComponent
   ],
   imports: [
     BrowserModule,
