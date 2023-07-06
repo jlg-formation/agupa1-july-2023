2096bb7167fc47426622e8bd69585ae6a331919a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-06 11:12:23 +0200

    dev: added behaviorsubject

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Article } from '../interfaces/article';
+import { BehaviorSubject } from 'rxjs';
+
+const articles: Article[] = [
+  { id: 'a1', name: 'Tournevis', price: 2.99, qty: 123 },
+  { id: 'a2', name: 'Pelle', price: 1.5, qty: 14 },
+];
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
-    { id: 'a1', name: 'Tournevis', price: 2.99, qty: 123 },
-    { id: 'a2', name: 'Pelle', price: 1.5, qty: 14 },
-  ];
-
+  articles$ = new BehaviorSubject<Article[]>(articles);
   constructor() {}
 }
