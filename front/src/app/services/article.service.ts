5bb8919bf2e786afefc71186f1feb51047e7af0c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-06 15:04:26 +0200

    dev: added action submit to add article

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Article } from '../interfaces/article';
-import { BehaviorSubject } from 'rxjs';
+import { Article, NewArticle } from '../interfaces/article';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 const articles: Article[] = [
   { id: 'a1', name: 'Tournevis', price: 2.99, qty: 123 },
@@ -12,5 +12,14 @@ const articles: Article[] = [
 })
 export class ArticleService {
   articles$ = new BehaviorSubject<Article[]>(articles);
+
   constructor() {}
+
+  add(newArticle: NewArticle): Observable<void> {
+    return of(undefined);
+  }
+
+  refresh(): Observable<void> {
+    throw new Error('Method not implemented.');
+  }
 }
--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { catchError, delay, of, switchMap } from 'rxjs';
+import { NewArticle } from 'src/app/interfaces/article';
+import { ArticleService } from 'src/app/services/article.service';
 
 @Component({
   selector: 'app-add',
@@ -8,10 +12,38 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./add.component.scss'],
 })
 export class AddComponent {
-  faPlus = faPlus;
   f = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+    name: new FormControl('Truc', [
+      Validators.required,
+      Validators.maxLength(10),
+    ]),
     price: new FormControl(0, [Validators.required]),
     qty: new FormControl(0, [Validators.required]),
   });
+  faPlus = faPlus;
+  errorMsg = '';
+
+  constructor(
+    private readonly articleService: ArticleService,
+    private readonly router: Router
+  ) {}
+
+  submit() {
+    console.log('submit');
+    const newArticle: NewArticle = this.f.value as NewArticle;
+
+    of(undefined)
+      .pipe(
+        delay(2000),
+        switchMap(() => this.articleService.add(newArticle)),
+        switchMap(() => this.articleService.refresh()),
+        switchMap(() => this.router.navigateByUrl('/stock')),
+        catchError((err) => {
+          console.log('err: ', err);
+          this.errorMsg = 'Erreur Technique';
+          return of(undefined);
+        })
+      )
+      .subscribe();
+  }
 }
