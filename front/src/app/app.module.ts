2c530d45889bde180f629849dc7406c1ccb21252
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-07 09:43:39 +0200

    dev: article refresh from back

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,20 +8,19 @@ import { LayoutModule } from './layout/layout.module';
 import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ArticleService } from './services/article.service';
+import { HttpArticleService } from './services/http-article.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    LegalComponent
-  ],
+  declarations: [AppComponent, HomeComponent, LegalComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     LayoutModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: ArticleService, useClass: HttpArticleService }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- front/src/app/services/http-article.service.ts
+++ front/src/app/services/http-article.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { ArticleService } from './article.service';
+import { Observable, delay, map, of, switchMap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Article } from '../interfaces/article';
+
+const url = 'http://localhost:3000/api/articles';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class HttpArticleService {
+export class HttpArticleService extends ArticleService {
+  constructor(private readonly http: HttpClient) {
+    super();
+    console.log('http article');
+  }
 
-  constructor() { }
+  override refresh(): Observable<void> {
+    return of(undefined).pipe(
+      delay(1000),
+      switchMap(() => {
+        return this.http.get<Article[]>(url);
+      }),
+      map((articles) => {
+        this.articles$.next(articles);
+      })
+    );
+  }
 }
