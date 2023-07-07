877fba2267d738b91973cd8b0fad315f0f7c2ece
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-07 15:03:54 +0200

    prod: no console.log

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,7 +1,14 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { isDevMode } from '@angular/core';
 
+if (!isDevMode()) {
+  console.log = () => {
+    // empty
+  };
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
