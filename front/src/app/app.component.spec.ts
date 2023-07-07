937a17c2658f27b80c019ed6879ad8addb9bdd64
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-07 12:09:51 +0200

    test: starting test

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, LayoutModule],
+      declarations: [AppComponent],
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -24,6 +27,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('front app is running!');
+    expect(compiled.querySelector('header span')?.textContent).toContain(
+      'Gestion Stock'
+    );
   });
 });
--- front/src/app/services/http-article.service.spec.ts
+++ front/src/app/services/http-article.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HttpArticleService } from './http-article.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('HttpArticleService', () => {
   let service: HttpArticleService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(HttpArticleService);
   });
 
