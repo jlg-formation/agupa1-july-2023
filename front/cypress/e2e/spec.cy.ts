4a37472b3143110bfeff9aabc9f53fb001f9e920
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-07 14:51:15 +0200

    e2e: test

--- front/cypress/e2e/spec.cy.ts
+++ front/cypress/e2e/spec.cy.ts
@@ -1,6 +1,19 @@
 describe('My First Test', () => {
   it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
-  })
-})
+    cy.visit('/');
+    cy.contains('Gestion Stock');
+    cy.contains('Mentions Légales');
+    cy.contains('Gérer efficacement votre stock');
+    cy.contains('a', 'Voir le stock').click();
+    cy.get('a[title="Ajouter"]').click();
+    const uuid = () => Cypress._.random(0, 1e6);
+    const id = uuid();
+    const testname = `o-${id}`;
+    cy.get('input').eq(0).clear().type(testname);
+    cy.get('input').eq(1).clear().type('1.56');
+    cy.get('input').eq(2).clear().type('45');
+    cy.contains('button', 'Ajouter').click();
+    cy.contains('table tbody tr td', testname).click();
+    cy.get('button[title="Supprimer"]').click();
+  });
+});
