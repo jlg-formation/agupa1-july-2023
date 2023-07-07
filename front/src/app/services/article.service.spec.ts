82a7c754c32ed9ad069cf683c9d099402bfc17e1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-07-07 13:52:46 +0200

    test: coverage

--- front/src/app/services/article.service.spec.ts
+++ front/src/app/services/article.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ArticleService } from './article.service';
+import { newArticle } from 'src/test/data';
 
 describe('ArticleService', () => {
   let service: ArticleService;
@@ -11,6 +12,21 @@ describe('ArticleService', () => {
   });
 
   it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should add an article', () => {
+    service.add(newArticle).subscribe();
+    expect(service).toBeTruthy();
+  });
+
+  it('should refresh', () => {
+    service.refresh().subscribe();
+    expect(service).toBeTruthy();
+  });
+
+  it('should remove', () => {
+    service.remove([]).subscribe();
     expect(service).toBeTruthy();
   });
 });
---
+++ front/src/test/data.ts
@@ -0,0 +1,7 @@
+import { NewArticle } from 'src/app/interfaces/article';
+
+export const newArticle: NewArticle = {
+  name: 'Truc',
+  price: 1.23,
+  qty: 10,
+};
